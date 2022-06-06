053063ebbd0f0ec75674fa8d2fbd3d19c4fba3ee
David Koop <dakoop@niu.edu>
2022-06-06 11:03:20 -0500

    Migrate jupyter-widget to a jupyterlab 3 prebuilt extension
    The main thing is modifying package.json and webpack.config.js to
    work with the jupyterlab builder. Since the previous build was
    just using webpack, we let the builder integrate the webpack config
    from that build with the labextension webpack. Required some changes
    to remove the original dist directory so everything just goes to a
    pydeck/labextension directory now.

--- modules/jupyter-widget/src/plugin.js
+++ modules/jupyter-widget/src/plugin.js
@@ -4,8 +4,7 @@
  * https://github.com/jupyter-widgets/widget-ts-cookiecutter/blob/51e9fed8687e3b9cf1ed2fd307b7675e864f89ae/%7B%7Bcookiecutter.github_project_name%7D%7D/src/plugin.ts
  */
 import {IJupyterWidgetRegistry} from '@jupyter-widgets/base';
-// eslint-disable-next-line import/no-unresolved
-import * as widgetExports from '../dist/index';
+import * as widgetExports from './index';
 
 const EXTENSION_ID = '@deck.gl/jupyter-widget:plugin';
 
@@ -18,6 +17,7 @@ const widgetPlugin = {
 
 export default widgetPlugin;
 function activateWidgetExtension(app, registry) {
+  console.log("deck.gl jupyter-widget MODULE_NAME:", widgetExports.MODULE_NAME, "MODULE_VERSION:", widgetExports.MODULE_VERSION);
   registry.registerWidget({
     name: widgetExports.MODULE_NAME,
     version: widgetExports.MODULE_VERSION,
